856f12a15ed3191c39fd9d1cfcd917ec05e9a710
Yixuan <yhou219@gmail.com>
2023-02-13 16:46:43 +1300

    load more icons

--- front-end/src/shared/helpers/load-icons.ts
+++ front-end/src/shared/helpers/load-icons.ts
@@ -1,4 +1,4 @@
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faCheck, faSpinner, faUsers } from "@fortawesome/free-solid-svg-icons"
+import { faCheck, faSpinner, faUsers, faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons"
 
-library.add(faCheck, faSpinner, faUsers)
+library.add(faCheck, faSpinner, faUsers, faChevronDown, faChevronUp)
